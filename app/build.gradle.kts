f8ced9e2dd339dfda662d60cd7a7e02c7bae1f80
Lannister <gns7775@naver.com>
2026-03-07 15:23:01 +0900

    release versionCode=2, versionName=1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.example.goheung"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
